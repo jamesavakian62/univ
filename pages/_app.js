6a1052e55c2cd689dcaebf024ab67ab456a910a4
James L Avakian <92414210+jamesavakian62@users.noreply.github.com>
2022-10-25 12:38:25 -0700

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css';
+import Mega from '../components/mega';
+import URL from '../components/url';
+
+function MyApp() {
+  return (
+    <>
+      <Mega />
+      {/* // return <Component {...pageProps} />; */}
+      <URL />
+    </>
+  );
+}
+
+export default MyApp;
